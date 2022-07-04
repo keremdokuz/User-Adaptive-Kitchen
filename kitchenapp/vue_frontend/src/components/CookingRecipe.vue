22a8b02c4828f0d952b78092c0ec13148133d54d
Simon Liu <ucpqc@student.kit.edu>
2022-07-04 16:22:53 +0200

    Added Snackbars

--- kitchenapp/vue_frontend/src/components/CookingRecipe.vue
+++ kitchenapp/vue_frontend/src/components/CookingRecipe.vue
@@ -7,23 +7,40 @@
       >
         {{ cookingStep.desc }}
         <small>{{ cookingStep.hint }}</small>
+        
       </v-stepper-step>
 
       <v-stepper-content :step="`${cookingStep.id}`">
         <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
-        <v-btn color="primary" @click="currentStep = cookingStep.id + 1">
+        <v-btn v-if="currentStep!=recipe.steps.length" color="primary" @click="currentStep = cookingStep.id + 1; snackbar = true">
           Continue
         </v-btn>
-        <v-btn @click="currentStep = cookingStep.id - 1" text> Go Back </v-btn>
+        <v-btn v-if="currentStep!=1" @click="currentStep = cookingStep.id - 1; snackbar = true" text> Go Back </v-btn>
+
+        <v-snackbar
+          v-model="snackbar"
+          :timeout="timeout"
+        >
+          {{ recipe.steps[currentStep - 1].message }}
+        </v-snackbar>
+
+        
+
       </v-stepper-content>
+      
+      
+
     </v-container>
   </v-stepper>
+  
 </template>
 
 <script>
 export default {
   data() {
     return {
+      snackbar: false,
+      timeout:2000,
       currentStep: 1,
       recipe: {
         title: "THIS DISH",
@@ -33,24 +50,28 @@ export default {
             desc: "Turn on stove",
             feature: "none",
             hint: "Watch out for your fingers!",
+            message: "Back at the beginning",
           },
           {
             id: 2,
             desc: "Put water in pot and start boiling",
             feature: "runningWater",
             hint: "Add some salt too.",
+            message: "Sound recognized",
           },
           {
             id: 3,
             desc: "Chop vegetables and put them into pot",
             feature: "chopping",
             hint: "Tomatoes work especially well.",
+            message: "Sound recognized",
           },
           {
             id: 4,
             desc: "Heat up last night's leftovers in microwave",
             feature: "microwave",
             hint: "One to two minutes.",
+            message: "Alarm set for 2 Minutes",
           },
         ],
       },
