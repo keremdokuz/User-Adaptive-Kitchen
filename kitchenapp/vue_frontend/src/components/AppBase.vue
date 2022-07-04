546608cd30cc1cc17c04f83edc4995855d81a727
ukjge <ukjge@student.kit.edu>
2022-07-04 09:52:00 +0200

    created a basic vue frontend which communicates with django

---
+++ kitchenapp/vue_frontend/src/components/AppBase.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <v-app-bar dense dark>
+      <v-toolbar-title>User Adaptive Kitchen</v-toolbar-title>
+
+      <v-toolbar-items>
+        <v-btn @click="getPrediction" class="ml-4" color="blue"
+          >Get Prediction</v-btn
+        >
+      </v-toolbar-items>
+
+      <v-card-title>
+        <v-card-text v-if="!isLoading">
+          prediction:{{ prediction }} confidence:{{ confidence }}
+        </v-card-text>
+        <v-progress-circular v-if="isLoading" indeterminate />
+      </v-card-title>
+    </v-app-bar>
+
+    <CookingRecipe />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import CookingRecipe from "./CookingRecipe";
+
+export default {
+  name: "App",
+
+  components: {
+    CookingRecipe,
+  },
+
+  computed: {
+    prediction() {
+      return this.currentPrediction ? this.currentPrediction : "None";
+    },
+    confidence() {
+      return this.currentConfidence > 0 ? `${this.currentConfidence}` : "None";
+    },
+  },
+
+  methods: {
+    async getPrediction() {
+      (this.isLoading = true),
+        axios
+          .get("http://127.0.0.1:8000/predict")
+          .then((res) => {
+            console.log(res);
+            this.currentPrediction = res.data["classLabel"];
+            this.currentConfidence = res.data["confidence"];
+            this.isLoading = false;
+          })
+          .catch((err) => console.log(err));
+    },
+  },
+
+  data: () => ({
+    currentPrediction: "",
+    currentConfidence: -1,
+    isLoading: false,
+  }),
+};
+</script>
