10b88b91653cd3bb282684fef702b0b0201b6e98
Johannes <ubfuf@student.kit.edu>
2022-07-07 17:27:27 +0200

    changed url routing

--- kitchenapp/frontend/urls.py
+++ kitchenapp/frontend/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import get_prediction, index, vue_test, list_available_microphones
 
 urlpatterns = [
-    path('', index),
+    path('old', index),
     path('predict', get_prediction),
-    path('vue', vue_test),
+    path('', vue_test),
     path('mics', list_available_microphones)
 ]
