40da31286320708655c3d1a12aca931f160d63ed
Simon Liu <ucpqc@student.kit.edu>
2022-07-20 15:55:50 +0200

    Created Test Recipe

--- kitchenapp/vue_frontend/src/components/CookingRecipe.vue
+++ kitchenapp/vue_frontend/src/components/CookingRecipe.vue
@@ -21,6 +21,7 @@
           </v-stepper-step>
 
           <v-stepper-content :step="`${cookingStep.id}`">
+
             <v-img
               v-if="cookingStep.picture_url"
               :src="cookingStep.picture_url"
@@ -76,6 +77,8 @@
 </template>
 
 <script>
+
+
 export default {
   props: {
     recipe: Object,
@@ -85,6 +88,40 @@ export default {
     features(cookingStep) {
       return cookingStep.feature.join(", ");
     },
+    timer_test () {
+      console.log("We got here");
+      document.getElementById("demo").innerHTML = "TEST";
+    },
+    timer(){
+      // Set the date we're counting down to
+      var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+      // Update the count down every 1 second
+      var x = setInterval(function() {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+          clearInterval(x);
+          document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+      }, 1000);
+    }
   },
   watch: {
     currentStep(value) {
